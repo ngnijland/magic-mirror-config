7be3230ceb72791583c55bba471577f16c530a80
Niek Nijland <ngnijland@gmail.com>
2020-04-13 20:06:34 +0200

    fix: sed command for linux

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,5 +7,5 @@ DIR="../MagicMirror/config"
 find $DIR \
   -exec bash -c 'cp ./config/config.js ../MagicMirror/config/config.js' \;
 
-grep -rl __MAGIC_MIRROR_OPEN_WEATHER_API_KEY__ $DIR | xargs sed -i "" "s|__MAGIC_MIRROR_OPEN_WEATHER_API_KEY__|$MAGIC_MIRROR_OPEN_WEATHER_API_KEY|g"
+grep -rl __MAGIC_MIRROR_OPEN_WEATHER_API_KEY__ $DIR | xargs sed -i "s|__MAGIC_MIRROR_OPEN_WEATHER_API_KEY__|$MAGIC_MIRROR_OPEN_WEATHER_API_KEY|g"
 
